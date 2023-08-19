357d305037867e0fb830a3edafd1abe20257ab61
harisahmar <harisahmar7@gmail.com>
2023-08-19 22:02:00 +0530

    Adding Morgan

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const app = express();
 const port = 3000;
 const taskController = require('./controller/taskcontroller');
 const dbConnector = require('./config/dbSetting');
+const morgan = require('morgan');
 
 //Declaring middleware
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+
+function middleWare(req, res, next){
+  if(Object.keys(req.body).length){
+  console.log("POST REQUEST------------->",req.body);  
+  }
+
+  if(Object.keys(req.query).length){
+   console.log("GET REQUEST------------->",req.query); 
+  }
+
+  next();
+}
+
+app.use(middleWare);
 
 //Declaring routes
 app.get('/api/v1/tasks', taskController.getTask);
